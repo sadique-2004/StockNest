03f316473afc25e29d66f1ddb5fbf603668daa3f
sadique-2004 <mds044234@gmail.com>
2025-04-27 21:20:06 +0530

    component Structure

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div className="App">
+      <h1>Welcome to the React App</h1>
+    </div>
+
+  </React.StrictMode>
+);
+
+
